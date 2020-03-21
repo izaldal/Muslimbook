dbb3c3fb604eccf5565499a68693999f82ca55e0
rezeq1 <58135379+izaldal@users.noreply.github.com>
2020-03-21 13:40:21 +0200

    connect to firebase

---
+++ routes.py
@@ -0,0 +1,13 @@
+import pyrebase as pb
+firebaseConfig = {
+  'apiKey': "AIzaSyDqJIXnDJ9gMT4k8bDdSkJJTk7PEDRHlUI",
+  'authDomain': "muslimbook-38f85.firebaseapp.com",
+  'databaseURL': "https://muslimbook-38f85.firebaseio.com",
+  'projectId': "muslimbook-38f85",
+  'storageBucket': "muslimbook-38f85.appspot.com",
+  'messagingSenderId': "847402124105",
+  'appId': "1:847402124105:web:b858c01727c15313891098",
+  'measurementId': "G-G8CFSF8G9H"
+}
+
+firebase=pb.initialize_app(firebaseConfig)
